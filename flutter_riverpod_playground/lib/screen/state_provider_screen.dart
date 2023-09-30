db4fa8015d69b46c6fa21e38bf31ad7a68a4b66f
pyowonsik <qqrtyu@gmail.com>
2023-09-30 23:52:54 +0900

    feat : stateProvider

--- flutter_riverpod_playground/lib/screen/state_provider_screen.dart
+++ flutter_riverpod_playground/lib/screen/state_provider_screen.dart
@@ -25,41 +25,10 @@ class StateProviderScreen extends ConsumerWidget {
             ),
             ElevatedButton(
               onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) => const _NextScreen(),
-                  ),
-                );
+                ref.read(numberProvider.notifier).state =
+                    ref.read(numberProvider.notifier).state - 1;
               },
-              child: const Text('Next'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _NextScreen extends ConsumerWidget {
-  const _NextScreen({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final provider = ref.watch(numberProvider);
-
-    return DefaultLayout(
-      title: 'NextScreen',
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(provider.toString()),
-            ElevatedButton(
-              onPressed: () {
-                ref.read(numberProvider.notifier).update((state) => state + 1);
-              },
-              child: const Text('Up'),
+              child: const Text('Down'),
             ),
           ],
         ),
